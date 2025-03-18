9ea3334e51874e03359adbbcaf266b01eb559518
magopoulou <stavroula.mg@gmail.com>
2025-03-18 17:28:54 +0200

    changed app text

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ COPY . .
 RUN npm run build
 
 FROM nginx
-EXPOSE 80
+#use that as the port that gets mapped for incoming traffic
+EXPOSE 80 
 COPY --from=builder /app/build /usr/share/nginx/html
 
--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I love you Baby
+          I was changed for the feature branch
         </p>
         <a
           className="App-link"
